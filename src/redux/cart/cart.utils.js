1eed6eb7f5f33105a22755c5da5b5e0132f8d8e0
Dimi-Dun-Morogh <darnelbro@gmail.com>
2020-11-11 21:06:18 +0200

    added increase/decrease/remove item functionality to checkout-item  component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -11,3 +11,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const removeCartItem = (cartItems, cartItemToRemove) => {
+  return cartItems.filter((item) => item.id !== cartItemToRemove.id);
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+  if (existingCartItem.quantity === 1) {
+    return removeCartItem(cartItems, cartItemToRemove);
+  }
+
+  return cartItems.map((cartItem) =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem,
+  );
+};
